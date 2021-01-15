4326833c0a030590f777bc8119a61bfdea513ffe
João Paulo Lima <jptick@gmail.com>
2021-01-15 10:19:30 -0300

    Create DsClients GET/ By ID

--- dsclient/backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
+++ dsclient/backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -3,11 +3,14 @@ package com.devsuperior.dsclient.services;
 import com.devsuperior.dsclient.dto.ClientDTO;
 import com.devsuperior.dsclient.entities.Client;
 import com.devsuperior.dsclient.repositories.ClientRepository;
+import com.devsuperior.dsclient.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -19,5 +22,12 @@ public class ClientService {
     public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
         Page<Client> list = repository.findAll(pageRequest);
         return list.map(ClientDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id){
+        Optional<Client> obj = repository.findById(id);
+        Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+        return new ClientDTO(entity);
     }
 }
