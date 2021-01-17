7ed6b8c7a2e38b1497a65bb6f8a454df39f52910
João Paulo <jptick@gmail.com>
2021-01-17 11:50:04 -0300

    Implement Lombok in Entities

--- dscatalog/backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ dscatalog/backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,10 +1,12 @@
 package com.devsuperior.dscatalog.entities;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.Objects;
 
+@Data
 @Entity
 @Table(name = "tb_category")
 public class Category implements Serializable {
@@ -37,42 +39,5 @@ public class Category implements Serializable {
     @PreUpdate
     public void preUpdate(){
         updatedAt = Instant.now();
-    }
-
-    public Category(){}
-
-    public Category(Long id, String name){
-        super();
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Category category = (Category) o;
-        return id.equals(category.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
     }
 }
