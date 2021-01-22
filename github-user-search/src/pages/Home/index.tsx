7e7fab58cf5ce28b385f995d891b7e21b8cad2be
João Paulo <jptick@gmail.com>
2021-01-22 17:59:08 -0300

    Building Search Page Layout

--- github-user-search/src/pages/Home/index.tsx
+++ github-user-search/src/pages/Home/index.tsx
@@ -4,36 +4,34 @@ import { Link } from 'react-router-dom';
 import './styles.scss';
 
 const Home = () => (
-  <div className="home-container">
-    <div className="row home-content">
-      <div className="col-6 home-text">
-        <h1 className="text-title">
-          Desafio do Capítulo 3,
-          <br />
-          Bootcamp DevSuperior
-        </h1>
-        <div className="container-info">
-          <p className="text-subtitle">
-            Bem-vindos ao desafio do capítulo 3 do Bootcamp DevSuperior.
-          </p>
-          <p className="text-subtitle">
-            Favor observar as instruções passadas no capítulo sobre a elaboração
-            desse projeto.
-          </p>
-          <p className="text-subtitle">
-            Este design foi adaptado a partir de Ant Design System for Figma, de
-            Mateusz Wierzbicki:
-            <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=someone@gmail.com">
-              antforfigma@gmail.com
-            </a>
-          </p>
-        </div>
-        <Link to="/products">
-          <Button text="Começar" />
-        </Link>
+  <div className="row home-content">
+    <div className="col-6 home-text">
+      <h1 className="text-title">
+        Desafio do Capítulo 3,
+        <br />
+        Bootcamp DevSuperior
+      </h1>
+      <div className="container-info">
+        <p className="text-subtitle">
+          Bem-vindos ao desafio do capítulo 3 do Bootcamp DevSuperior.
+        </p>
+        <p className="text-subtitle">
+          Favor observar as instruções passadas no capítulo sobre a elaboração
+          desse projeto.
+        </p>
+        <p className="text-subtitle">
+          Este design foi adaptado a partir de Ant Design System for Figma, de
+          Mateusz Wierzbicki:
+          <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=someone@gmail.com">
+            antforfigma@gmail.com
+          </a>
+        </p>
       </div>
-      <div className="col-6"></div>
+      <Link to="/search">
+        <Button text="Começar" />
+      </Link>
     </div>
+    <div className="col-6"></div>
   </div>
 );
 
--- github-user-search/src/pages/Search/index.tsx
+++ github-user-search/src/pages/Search/index.tsx
@@ -1,11 +1,22 @@
+import Button from 'core/components/Button';
 import React from 'react';
 
 import './styles.scss';
 
 const Search = () => {
   return (
-    <div className="catalog-container">
-      <h1 className="catalog-title">Encontre um perfil no Github</h1>
+    <div className="search-container">
+      <div className="form-search">
+        <label htmlFor="search">Encontre um perfil no Github</label>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          className="form-control"
+          placeholder="Usuário Github"
+        />
+        <Button text="Encontrar" />
+      </div>
     </div>
   );
 };
