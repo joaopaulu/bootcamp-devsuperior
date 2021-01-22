330781f9c659d3320c65b177a3c4a79bdb296a2b
João Paulo <jptick@gmail.com>
2021-01-22 16:19:28 -0300

    Init Challenge React Github User Search

---
+++ github-user-search/src/pages/Search/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.scss';
+
+const Search = () => {
+  return (
+    <div className="catalog-container">
+      <h1 className="catalog-title">Encontre um perfil no Github</h1>
+    </div>
+  );
+};
+
+export default Search;
