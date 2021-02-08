4c2b29bd63575024749c9e58264b6cd8c0756e0e
João Paulo <jptick@gmail.com>
2021-02-08 10:14:14 -0300

    Create User CRUD

---
+++ dscatalog/backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dscatalog.dto;
+
+import com.devsuperior.dscatalog.entities.User;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserInsertDTO extends UserDTO{
+    private static final long serialVersionUID = 1L;
+
+    private String password;
+
+    UserInsertDTO(){
+        super();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
