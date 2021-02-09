162c8a6648e3bd2c81d8f0e18ee6ae87955b721c
João Paulo <jptick@gmail.com>
2021-02-09 06:33:13 -0300

    Custom ConstraitValidator

--- dscatalog/backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
+++ dscatalog/backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -1,12 +1,8 @@
 package com.devsuperior.dscatalog.dto;
 
-import com.devsuperior.dscatalog.entities.User;
-import lombok.Data;
+import com.devsuperior.dscatalog.services.validation.UserInsertValid;
 
-import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
-
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
     private static final long serialVersionUID = 1L;
 
--- dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/UserService.java
+++ dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/UserService.java
@@ -1,13 +1,10 @@
 package com.devsuperior.dscatalog.services;
 
-import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.dto.RoleDTO;
 import com.devsuperior.dscatalog.dto.UserDTO;
 import com.devsuperior.dscatalog.dto.UserInsertDTO;
-import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Role;
 import com.devsuperior.dscatalog.entities.User;
-import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import com.devsuperior.dscatalog.repositories.RoleRepository;
 import com.devsuperior.dscatalog.repositories.UserRepository;
 import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
