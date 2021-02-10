43c0bbe94f896133695ca7ec0b1c2ded79f42bcd
João Paulo <jptick@gmail.com>
2021-02-09 21:31:38 -0300

    Setting Spring Security and JWT Authorization

--- dscatalog/backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ dscatalog/backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -1,16 +1,21 @@
 package com.devsuperior.dscatalog.entities;
 
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Data
 @Entity
 @Table(name= "tb_user")
-public class User implements Serializable{
+public class User implements UserDetails, Serializable{
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -29,4 +34,35 @@ public class User implements Serializable{
             joinColumns = @JoinColumn(name= "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
