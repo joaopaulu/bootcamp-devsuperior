f04eaad160320ab50072035f68020eabf6212ca4
João Paulo Lima <jptick@gmail.com>
2021-02-11 17:53:25 -0300

    Update Product Filter

--- dscatalog/backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
+++ dscatalog/backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
@@ -16,7 +16,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     /*@Query("SELECT obj FROM Product obj JOIN FETCH obj.categories WHERE obj IN :products")
     List<Product> findProductsCategories(List<Product> products);*/
 
-    @Query("SELECT obj FROM Product obj INNER JOIN obj.categories cats WHERE "
-        + ":category IN cats")
-    Page<Product> find(Category category, Pageable pageable);
+    @Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats WHERE "
+            + "(COALESCE(:categories) IS NULL OR cats IN :categories) AND "
+            + "(LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%')))")
+    Page<Product> find(List<Category> categories, String name, Pageable pageable);
 }
--- dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -17,6 +17,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -30,11 +32,10 @@ public class ProductService {
     private CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> find(Long categoryId, PageRequest pageRequest) {
-        Category category = (categoryId == 0) ? null : categoryRepository.getOne(categoryId);
-        Page<Product> page = repository.find(category, pageRequest);
-        //repository.findProductsCategories(page.stream().collect(Collectors.toList()));
-        return page.map(ProductDTO::new);
+    public Page<ProductDTO> findAllPaged(Long categoryId, String name, PageRequest pageRequest) {
+        List<Category> categories = (categoryId == 0) ? null : Arrays.asList(categoryRepository.getOne(categoryId));
+        Page<Product> list = repository.find(categories, name, pageRequest);
+        return list.map(ProductDTO::new);
     }
 
     @Transactional(readOnly = true)
