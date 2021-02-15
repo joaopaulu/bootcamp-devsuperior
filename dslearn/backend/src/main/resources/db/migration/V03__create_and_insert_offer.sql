dfa5beeaa0b8fe10afac74863dd2e63811e37b02
João Paulo <jptick@gmail.com>
2021-02-15 19:23:12 -0300

    Create Offer Entity and Migration

---
+++ dslearn/backend/src/main/resources/db/migration/V03__create_and_insert_offer.sql
@@ -0,0 +1,10 @@
+create table tb_offer (
+id bigserial not null,
+edition varchar(10) not null,
+start_Moment TIMESTAMP WITHOUT TIME ZONE,
+end_Moment TIMESTAMP WITHOUT TIME ZONE,
+course_id int,
+primary key (id));
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
