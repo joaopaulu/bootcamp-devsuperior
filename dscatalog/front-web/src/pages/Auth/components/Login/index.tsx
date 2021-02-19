19ceab3e4f6a376e18d90dd77f9549c4ef5f4605
João Paulo <jptick@gmail.com>
2021-02-19 16:18:42 -0300

    Update Login Form Layout

--- dscatalog/front-web/src/pages/Auth/components/Login/index.tsx
+++ dscatalog/front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,4 +1,6 @@
+import ButtonIcon from 'core/components/Buttonicon';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import AuthCard from '../Card';
 import './styles.scss';
 
@@ -6,7 +8,28 @@ const Login = () => {
   return (
     <AuthCard title="login">
       <form className="login-form">
-        <h1>Formulário de Login</h1>
+        <input
+          type="email"
+          className="form-control input-base input-bt30"
+          placeholder="E-mail"
+        />
+        <input
+          type="password"
+          className="form-control input-base"
+          placeholder="Senha"
+        />
+        <Link to="/admin/auth/recover" className="login-link-recover">
+          Esqueci a senha?
+        </Link>
+        <div className="login-submit">
+          <ButtonIcon text="logar" />
+        </div>
+        <div className="text-center">
+          <span className="not-registered">Não tem cadastro?</span>
+          <Link to="/admin/auth/register" className="login-link-register">
+            CADASTRAR
+          </Link>
+        </div>
       </form>
     </AuthCard>
   );
