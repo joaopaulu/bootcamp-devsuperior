dc72ef9fd57f3d7b6a5e134f72f6f7754194026a
João Paulo <jptick@gmail.com>
2021-02-23 20:35:27 -0300

    Add product filters

--- dscatalog/front-web/src/pages/Catalog/index.tsx
+++ dscatalog/front-web/src/pages/Catalog/index.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ProductsResponse } from 'core/types/Product';
 import { makeRequest } from 'core/utils/request';
 import ProductCard from './components/ProductCard';
 import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import Pagination from 'core/components/Pagination';
+import ProductFilters, { FilterForm } from 'core/components/ProductFilters';
 import './styles.scss';
 
 const Catalog = () => {
@@ -12,20 +13,32 @@ const Catalog = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [activePage, setActivePage] = useState(0);
 
+  const getProducts = useCallback(
+    (filter?: FilterForm) => {
+      const params = {
+        page: activePage,
+        linesPerPage: 12,
+        name: filter?.name,
+        categoryId: filter?.categoryId,
+      };
+      setIsLoading(true);
+      makeRequest({ url: '/products', params })
+        .then(response => setProductsResponse(response.data))
+        .finally(() => setIsLoading(false));
+    },
+    [activePage],
+  );
+
   useEffect(() => {
-    const params = {
-      page: activePage,
-      linesPerPage: 12,
-    };
-    setIsLoading(true);
-    makeRequest({ url: '/products', params })
-      .then(response => setProductsResponse(response.data))
-      .finally(() => setIsLoading(false));
-  }, [activePage]);
+    getProducts();
+  }, [getProducts]);
 
   return (
     <div className="catalog-container">
-      <h1 className="catalog-title">Catálogo de produtos</h1>
+      <div className="d-flex justify-content-between">
+        <h1 className="catalog-title">Catálogo de produtos</h1>
+        <ProductFilters onSearch={filter => getProducts(filter)} />
+      </div>
       <div className="catalog-products">
         {isLoading ? (
           <ProductCardLoader />
