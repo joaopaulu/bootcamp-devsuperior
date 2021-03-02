a70f9d9e87f65e3c089e392b2e3b740b7f2577fa
João Paulo <jptick@gmail.com>
2021-03-02 15:03:24 -0300

    Init Spring IO Tests

---
+++ tutorial-spring-web/src/main/java/br/com/devsuperior/tutorialspringweb/TutorialSpringWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.devsuperior.tutorialspringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialSpringWebApplication.class, args);
+	}
+
+}
---
+++ tutorial-spring-web/src/test/java/br/com/devsuperior/tutorialspringweb/TutorialSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devsuperior.tutorialspringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialSpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
