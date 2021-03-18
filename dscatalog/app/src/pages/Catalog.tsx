02fd280141cb79ca4fabf06dc16876720e330e6c
João Paulo <jptick@gmail.com>
2021-03-18 08:15:56 -0300

    Styling ProductCard Component

--- dscatalog/app/src/pages/Catalog.tsx
+++ dscatalog/app/src/pages/Catalog.tsx
@@ -1,11 +1,49 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, Text } from 'react-native';
+import { ProductCard } from '../components';
+
+import productImg from '../assets/product.png';
+import { theme } from '../styles';
+const products = [
+  {
+    id: 1,
+    imgUrl: productImg,
+    name: 'Computador Desktop - Intel Core i7',
+    price: 2299.9,
+  },
+  {
+    id: 2,
+    imgUrl: productImg,
+    name: 'Computador Desktop - Intel Core i7',
+    price: 2299.9,
+  },
+  {
+    id: 3,
+    imgUrl: productImg,
+    name: 'Computador Desktop - Intel Core i7',
+    price: 2299.9,
+  },
+  {
+    id: 4,
+    imgUrl: productImg,
+    name: 'Computador Desktop - Intel Core i7',
+    price: 2299.9,
+  },
+  {
+    id: 5,
+    imgUrl: productImg,
+    name: 'Computador Desktop - Intel Core i7',
+    price: 2299.9,
+  },
+];
 
 const Catalog: React.FC = () => {
   return (
-    <View>
-      <Text>Bem vindo ao Catalogo</Text>
-    </View>
+    <ScrollView contentContainerStyle={theme.scrollContainer}>
+      {products.map(product => (
+        <ProductCard key={product.id} {...product} />
+      ))}
+    </ScrollView>
   );
 };
 
--- dscatalog/app/src/styles/index.ts
+++ dscatalog/app/src/styles/index.ts
@@ -33,6 +33,20 @@ const text = StyleSheet.create({
     color: colors.white,
     marginLeft: 25,
   },
+  productName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  currency: {
+    fontSize: 16,
+    fontWeight: '400',
+    color: colors.mediumGray,
+  },
+  productPrice: {
+    fontSize: 30,
+    color: colors.primary,
+    fontWeight: 'bold',
+  },
 });
 
 const theme = StyleSheet.create({
@@ -42,6 +56,7 @@ const theme = StyleSheet.create({
     alignItems: 'center',
     padding: 20,
   },
+
   card: {
     width: '100%',
     height: '100%',
@@ -79,6 +94,34 @@ const theme = StyleSheet.create({
     borderBottomRightRadius: 10,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  scrollContainer: {
+    padding: 10,
+  },
+  productCard: {
+    width: '100%',
+    backgroundColor: colors.white,
+    borderRadius: 10,
+    shadowColor: colors.black,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    marginVertical: 10,
+  },
+  productDescription: {
+    width: '100%',
+    padding: 20,
+    borderTopColor: colors.lightGray,
+    borderTopWidth: 1,
+  },
+  priceContainer: {
+    flexDirection: 'row',
+    marginTop: 10,
   },
 });
 
